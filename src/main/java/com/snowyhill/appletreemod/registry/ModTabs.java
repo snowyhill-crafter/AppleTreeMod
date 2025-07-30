7d97c4a0ec50c30d97e57c8ad0f7494a73346bb1
snowyhill <snowyhill.highland@gmail.com>
2025-07-30 22:58:14 +0900

    Add files via upload

---
+++ src/main/java/com/snowyhill/appletreemod/registry/ModTabs.java
@@ -0,0 +1,36 @@
+package com.snowyhill.appletreemod.registry;
+
+import com.snowyhill.appletreemod.AppleTreeMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class ModTabs {
+    //レジストリを作る
+    public static final DeferredRegister<CreativeModeTab> TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AppleTreeMod.MOD_ID);
+
+    //レジストリにタブを追加
+    public static final RegistryObject<CreativeModeTab> APPLETREEMOD_TAB = TABS.register("appletreemod_tab",
+    () -> CreativeModeTab.builder()
+            .title(Component.translatable("creativetabs.appletreemod_tab"))
+    .icon(ModItems.APPLE_SAPLING.get()::getDefaultInstance)
+            .displayItems(((pParameters, pOutput) -> {
+
+                pOutput.accept(ModItems.APPLE_SAPLING.get());
+                pOutput.accept(ModItems.APPLE_LEAVES_ITEM.get());
+                pOutput.accept(ModItems.ORNAMENTAL_APPLE_FLOWER_LEAVES_ITEM.get());
+                pOutput.accept(ModItems.ORNAMENTAL_APPLE_UNRIPE_LEAVES_ITEM.get());
+                pOutput.accept(ModItems.ORNAMENTAL_APPLE_FRUIT_LEAVES_ITEM.get());
+
+            }))
+            .build());
+
+    public static void register(IEventBus eventBus) {
+        TABS.register(eventBus);
+    }
+}
